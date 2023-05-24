22e02a6434255315ae2fba523dd93bf5945b2ebf
MaximMaximS <sklenicka.maxim@gmail.com>
2023-05-24 10:24:13 +0200

    Add Docker

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 // @ts-check
+
+const CSP = `
+default-src 'self';
+script-src 'unsafe-inline' 'unsafe-eval' 'self';
+style-src 'unsafe-inline' 'self';
+object-src 'none';
+base-uri 'self';
+connect-src 'self';
+font-src 'self';
+frame-src 'self';
+img-src 'self';
+manifest-src 'self';
+media-src 'self';
+worker-src 'none';
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -6,6 +22,66 @@ const nextConfig = {
     typedRoutes: true,
   },
   reactStrictMode: true,
+  poweredByHeader: false,
+  output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: CSP.replace(/\s+/g, " ").trim(),
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Resource-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Origin-Agent-Cluster",
+            value: "?1",
+          },
+          {
+            key: "Refferrer-Policy",
+            value: "no-referrer",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Download-Options",
+            value: "noopen",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+
+          {
+            key: "X-Permitted-Cross-Domain-Policies",
+            value: "none",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "0",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
