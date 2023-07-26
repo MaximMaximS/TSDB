6e33ea50b51ac71c00fc6562838b086acf161e86
MaximMaximS <sklenicka.maxim@gmail.com>
2023-07-26 14:21:36 +0200

    Fix header button

--- src/components/layout/site-header/header-button.tsx
+++ src/components/layout/site-header/header-button.tsx
@@ -1,13 +1,15 @@
 import { Button } from "@/components/ui/button";
 
+export const props = {
+  variant: "ghost",
+  size: "sm",
+  className: "w-9 px-0",
+} as const;
+
 export default function HeaderButton({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <Button variant="ghost" size="sm" className="w-9 px-0">
-      {children}
-    </Button>
-  );
+  return <Button {...props}>{children}</Button>;
 }
--- src/components/layout/site-header/mode-toggle.tsx
+++ src/components/layout/site-header/mode-toggle.tsx
@@ -3,6 +3,7 @@
 import { Laptop, Moon, SunMedium } from "lucide-react";
 import { useTheme } from "next-themes";
 
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,7 +11,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import HeaderButton from "./header-button";
+import { props } from "./header-button";
 
 export default function ModeToggle() {
   const { setTheme } = useTheme();
@@ -18,11 +19,11 @@ export default function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <HeaderButton>
+        <Button {...props}>
           <SunMedium className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
-        </HeaderButton>
+        </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
